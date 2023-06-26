af1e49058efc1aa33378139628ebf4fd639cd2ff
José Burgos <josem.123burgo@gmail.com>
2023-06-25 21:16:15 -0400

    feat: clap cli with side, max and count flags

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,68 @@
+use clap::{arg, command, Parser};
+use clap_num::number_range;
 use rand::Rng;
 
-fn main() {
-    const DICE_COUNT: usize = 6;
-    let mut thread_rng = rand::thread_rng();
+fn in_side_range(s: &str) -> Result<u8, String> {
+    number_range(s, 1, 6)
+}
 
-    let mut rounds = 0;
-    let value = loop {
-        rounds += 1;
-        let first = thread_rng.gen_range(1u8..=6);
-        println!("start");
-        if [0u8; DICE_COUNT - 1]
-            .iter()
-            .map(|_| thread_rng.clone().gen_range(1..=6))
-            .inspect(|dice| println!("{dice}"))
-            .all(|dice| dice == first)
-        {
-            break first;
+/// Simple program to roll a count of dice until they all are equal
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Which specific side must be repeated a count amount of times
+    #[arg(short, long, value_parser=in_side_range)]
+    side: Option<u8>,
+
+    /// Max amount of rolls
+    #[arg(short, long, default_value_t = 1_000_000)]
+    max: u32,
+
+    /// Number of dice rolled
+    #[arg(short, long, default_value_t = 6)]
+    count: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+    let mut thread_rng = rand::thread_rng();
+    let mut dice_rolled = 0;
+    let mut generate_side = || {
+        if dice_rolled == args.max {
+            None
+        } else {
+            dice_rolled += 1;
+            Some(thread_rng.gen_range(1..=6))
         }
     };
-    println!("It took {rounds} rounds to get six {value}'s");
+
+    let maybe = 'outer: loop {
+        let Some(first) = generate_side() else {
+            break None;
+        };
+        for _ in 0..args.count {
+            let Some(other) = generate_side() else {
+                break 'outer None;
+            };
+            if other != first {
+                continue 'outer;
+            }
+        }
+        match args.side {
+            Some(side) if side == first => break Some(first),
+            Some(_) => {}
+            None => break Some(first),
+        }
+    };
+
+    match maybe {
+        Some(value) => println!(
+            "Dice fell on {value} {} times after after rolling {dice_rolled} dice",
+            args.count
+        ),
+        None => println!(
+            "Dice failed to fall {} on a row on the same side, {dice_rolled}",
+            args.count
+        ),
+    }
 }
